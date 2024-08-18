48c9266c6cdb8b17e60f46ad5a3365bfc02059e9
djokcho <probgdjokcho@gmail.com>
2024-08-18 17:03:40 +0300

    Common: Enable Lk1st menu

--- lk2nd/project/lk1st.mk
+++ lk2nd/project/lk1st.mk
@@ -1,13 +1,12 @@
 # SPDX-License-Identifier: BSD-3-Clause
 LK2ND_PROJECT := lk1st
+LK2ND_DISPLAY ?= cont-splash
 include lk2nd/project/base.mk
+ENABLE_FBCON_DISPLAY_MSG := 1
 
 # The device module is useless on lk1st if no DTB is bundled
 ifneq ($(LK2ND_BUNDLE_DTB),)
 MODULES += lk2nd/device
-ifneq ($(ENABLE_FBCON_DISPLAY_MSG),1)
-MODULES += $(if $(filter $(MODULES), lk2nd/display), lk2nd/device/menu)
-endif
 else
 $(info NOTE: Device module is disabled without bundling a device DTB (LK2ND_BUNDLE_DTB))
 endif
